d42a6a0e5f171f0f5a01d9d49ee986ae2d511d82
pavankumar19992208 <pavankumar19992208@gmail.com>
2024-09-10 20:54:00 +0530

    ppp--verified

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, APIRouter, HTTPException, Request
-import google.generativeai as genai
+from GEN_AI.assist import assist_router
 import os
-from fastapi import FastAPI, APIRouter
 from web.ps_update import ps_update_router
 from web.getList import get_list_router
 from web.psdetails import ps_details_router
@@ -9,7 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from details.Dregister import Dregister_router
 app = FastAPI(docs_url="/docs")
 from runtests.runtests_router import runtests_router
-from content_generator import generate_content
+
 origins = [
     "*",  # Allow all origins
 ]
@@ -24,31 +23,12 @@ app.add_middleware(
 
 
 
-assist_router = APIRouter()
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@assist_router.post("/assist")
-async def assist(request: Request):
-    try:
-        # Get the data from the request
-        data = await request.json()
-        code = data.get("code")
-        problem_statement = data.get("problemStatement")
-        error = data.get("results")  # Assuming 'results' contains error information
 
-        # Prepare the prompt for the Gemini API
-        prompt = f"Code: {code}\nProblem Statement: {problem_statement}\nError: {error}\n"
-
-        # Use the Gemini API to generate content
-        response_text = generate_content(prompt)
-        print(response_text)
-        # Return the response from the Gemini API to the frontend
-        return {"response": response_text}
-    except Exception as e:
-        print(f"Error: {e}")
-        raise HTTPException(status_code=500, detail="Internal Server Error")
 
 app.include_router(runtests_router)
 app.include_router(ps_update_router)
--- temp_code.py
+++ temp_code.py
@@ -1,10 +1,18 @@
 def bubble_sort(arr):
-    return sorted(arr)
+
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                
+    return arr
 
 # Driver code
 if __name__ == '__main__':
     import sys
     input_data = sys.stdin.read().strip()
-    input_list = eval(input_data)  # Convert the input string to a list
+    input_list = eval(input_data)  
+    # Convert the input string to a list
     result = bubble_sort(input_list)
     print(result)
